b0fce2c9db209d56e6bf97f1eb4e398f1ede6de4
Adam <adambacha95@gmail.com>
2025-01-13 09:48:36 +0100

    first

---
+++ src/Controller/ProduitController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ProduitRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class ProduitController extends AbstractController
+{
+    #[Route('/homepage', name: 'app_produit')]
+    public function index(ProduitRepository $repository): Response
+    {
+
+        $produit = $repository->findAll();
+        return $this->render('produit/index.html.twig', [
+            'produits' => $produit,
+        ]);
+    }
+    
+    
+    #[Route('/homepage/{alias}', name: 'produit_show')]
+public function show(string $alias, ProduitRepository $repository): Response
+{
+    $product = $repository->findOneBy(['alias' => $alias]);
+    return $this->render('produit/show.html.twig', [
+        'produit' => $product,
+    ]);
+}
+
+}
+
---
+++ src/DataFixtures/AppFixtures.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Produit;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AppFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $produits = [
+            ['Lance-flamme', 1000, 30, "lanceflamme.jpg", true,"lanceflamme"],
+            ['Extincteur', 120, 10, "extincteur.jpg", false,"extincteur"],
+            ['Couteau de cuisine', 25, 0.5, "couteaudecuisine.jpg", true,"couteaudecuisine"],
+            ['Trousse de secours', 15, 1, "troussedesecour.jpg", false,"troussedesecour"],
+            ['Sac à dos', 50, 2, "sacados.jpg", false,"sacados"],
+            ['Barret M82', 800, 15, "fusildechasse.jpg", true,"fusildechasse"],
+            ['Jerrican d\'essence', 60, 20, "jerricanessence.jpg", true,"jerricanessence"],
+            ['Boîte de conserve', 5, 0.4, "boitedeconserve.jpg", false,"boitedeconserve"],
+            ['Voiture pour enfant', 45000, 1800, "voitureenfant.jpg", false,"voitureenfant"],
+            ['Bombe lacrymogène', 40, 0.3, "bombelacrymogene.jpg", true, "bombelacrymogene"]
+        ];
+        foreach ($produits as [$nom, $prix, $poids, $image, $dangereux, $alias]) {
+                $produit = new Produit();
+                $produit->setNom($nom);
+                $produit->setPrix($prix);
+                $produit->setPoids($poids);
+                $produit->setImage("/image/produit/" . $image);
+                $produit->setDangereux($dangereux);
+                $produit->setAlias($alias);
+                $manager->persist($produit);
+            }
+        $manager->flush();
+    }
+}
